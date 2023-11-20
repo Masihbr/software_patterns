540408057ee5347e79be8d5d2f14eb0cb11f6885
seyyedAlirezaGhazanfari <alireza79.ghazanfari@gmail.com>
2023-11-20 07:39:30 +0330

    Correct state test
    Change format of state test in product to implicit form and check type of state.

--- src/tests/ProductTest.java
+++ src/tests/ProductTest.java
@@ -3,6 +3,8 @@ package src.tests;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import src.Product;
+import src.states.DeliveredState;
+import src.states.TransitState;
 import src.strategies.ExpressStrategy;
 import src.strategies.StandardStrategy;
 
@@ -13,7 +15,7 @@ class ProductTest {
     @DisplayName("set weight test")
     void setWeightTest() {
         double weight = 64;
-        Product pac = new Product(weight, null, null);
+        Product pac = new Product(weight, null);
         assertEquals(weight, pac.getWeight());
         pac.setWeight(weight);
         assertEquals(weight, pac.getWeight());
@@ -23,7 +25,7 @@ class ProductTest {
     @DisplayName("get weight test")
     void getWeightTest() {
         double weight = 53.2;
-        Product product = new Product(weight, null, null);
+        Product product = new Product(weight, null);
         assertEquals(weight, product.getWeight());
     }
 
@@ -31,7 +33,7 @@ class ProductTest {
     @DisplayName("strategy set for product")
     void setStrategyTest() {
         StandardStrategy s = new StandardStrategy();
-        Product product = new Product(12.2, s, null);
+        Product product = new Product(12.2, s);
         assertEquals(s, product.getTransferStrategy());
         ExpressStrategy exp = new ExpressStrategy();
         product.setTransferStrategy(exp);
@@ -41,11 +43,9 @@ class ProductTest {
     @Test
     @DisplayName("state for product")
     void setStateTest() {
-        TransitState state = new TransitState();
-        Product product = new Product(10, null, state);
-        assertEquals(state, product.getState());
-        DeliveredState st = new DeliveredState();
-        product.setState(st);
-        assertEquals(st, product.getState());
+        Product product = new Product(10, null);
+        assertTrue(product.getState() instanceof TransitState);
+        product.changeState();
+        assertTrue(product.getState() instanceof DeliveredState);
     }
 }
