640380d8575e42529f0a1c02d13d1c211690b71f
Masihbr <masihbr@gmail.com>
2023-11-22 13:44:53 +0330

    Make messages user-friendly

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 package src;
 
 import src.states.DeliveredState;
-import src.states.StartState;
 import src.states.State;
 import src.states.TransitState;
 import src.strategies.ExpressStrategy;
@@ -13,17 +12,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Product product = Product.getInstance();
-        System.out.println("please give weight:");
+        System.out.println("Please enter the weight of the product in kilograms:");
         Scanner scanner = new Scanner(System.in);
         double weight = scanner.nextDouble();
         product.setWeight(weight);
         do {
-            System.out.println("give strategy for transferring: (1 standard 2 express)");
+            System.out.println("Please select a strategy for transferring the product: (1 for standard, 2 for express):");
             int strategy = scanner.nextInt();
-            System.out.println("give state of product: 1 in transit 2 delivered");
+            System.out.println("Please enter the current state of the product: (1 for in transit, 2 for delivered):");
             int state = scanner.nextInt();
             Strategy strategyObj = null;
-            if (strategy == 1){
+            if (strategy == 1) {
                 strategyObj = new StandardStrategy();
             } else if (strategy == 2) {
                 strategyObj = new ExpressStrategy();
@@ -38,7 +37,7 @@ public class Main {
                 product.changeState(stateObj);
             }
         } while (!product.isFinished());
-        System.out.println("price:");
+        System.out.println("The total cost of transferring the product is:");
         System.out.println(product.getCost());
     }
 }
